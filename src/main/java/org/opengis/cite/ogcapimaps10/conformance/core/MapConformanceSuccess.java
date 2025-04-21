015c8e4c4a5e64b6abaa13c9cdf1feb7621dc63b
LibbyShihh <libby890723@gmail.com>
2025-04-21 16:18:32 +0800

    #3 Implement A.1.3. Abstract Test for Requirement Map Conformance Success

---
+++ src/main/java/org/opengis/cite/ogcapimaps10/conformance/core/MapConformanceSuccess.java
@@ -0,0 +1,62 @@
+package org.opengis.cite.ogcapimaps10.conformance.core;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.opengis.cite.ogcapimaps10.conformance.CommonFixture;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * <pre>
+ * Abstract test A.1.3
+ *
+ * Identifier: /conf/core/conformance-success
+ * Requirement: Requirement 3: /req/core/conformance-success
+ * Test purpose: For implementations having a mechanism to advertise conformance classes,
+ * verify that it reports conformance to this Standard correctly.
+ *
+ * Test method:
+ * Given: a conformance resource in a recognized format, such as the OGC API JSON /conformance resource
+ * When: retrieving that resource from the API endpoint
+ * Then: assert that the list of conformance classes includes at minimum:
+ *   https://www.opengis.net/spec/ogcapi-maps-1/1.0/req/core
+ * </pre>
+ */
+public class MapConformanceSuccess extends CommonFixture {
+
+	@Test(description = "Implements A.1.3. Abstract Test for Requirement Map Conformance Success (Requirement /req/core/conformance-success)")
+	public void verifyConformanceClassPresence() throws Exception {
+		ObjectMapper objectMapper = new ObjectMapper();
+		String conformanceUrl = rootUri.toString() + "/conformance";
+
+		Map<String, Object> responseMap = objectMapper.readValue(new URL(conformanceUrl), Map.class);
+
+		Object conformsToObj = responseMap.get("conformsTo");
+		Assert.assertNotNull(conformsToObj, "'conformsTo' is missing in response.");
+
+		List<?> conformsToList = (List<?>) conformsToObj;
+
+		boolean found = false;
+		for (Object o : conformsToList) {
+			if (o != null && o.toString().equals("http://www.opengis.net/spec/ogcapi-maps-1/1.0/req/core")) {
+				found = true;
+				break;
+			}
+		}
+
+		if (!found) {
+			System.out.println("Returned conformsTo entries:");
+			for (Object o : conformsToList) {
+				System.out.println(" - " + o);
+			}
+		}
+
+		Assert.assertTrue(found, "The required conformance class 'req/core' is not declared.");
+	}
+
+}
---
+++ src/test/java/org/opengis/cite/ogcapimaps10/conformance/core/MapConformanceSuccessIT.java
@@ -0,0 +1,41 @@
+package org.opengis.cite.ogcapimaps10.conformance.core;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.net.URI;
+
+import org.testng.ITestContext;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import org.testng.ISuite;
+
+/**
+ * Integration Test for MapConformanceSuccess
+ */
+public class MapConformanceSuccessIT {
+
+	private static ITestContext testContext;
+
+	private static ISuite suite;
+
+	private static URI rootUri;
+
+	@BeforeClass
+	public static void initTestFixture() throws Exception {
+		testContext = mock(ITestContext.class);
+		suite = mock(ISuite.class);
+		when(testContext.getSuite()).thenReturn(suite);
+
+		rootUri = new URI("http://localhost:5000");
+		when(suite.getAttribute("iut")).thenReturn(rootUri);
+	}
+
+	@Test
+	public void testMapConformanceSuccess() throws Exception {
+		MapConformanceSuccess conformanceTest = new MapConformanceSuccess();
+		conformanceTest.initCommonFixture(testContext);
+
+		conformanceTest.verifyConformanceClassPresence();
+	}
+}
